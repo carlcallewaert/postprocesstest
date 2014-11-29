6195aced0ac8af22738960d0865d244f29d191df
carlcallewaert <carl@unity3d.com>
2014-11-29 10:31:24 -0800

    frist commit

---
+++ postpro/Assets/Plugins/iCloud/demo/iCloudEventListener.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class iCloudEventListener : MonoBehaviour
+{
+#if UNITY_IPHONE
+	void OnEnable()
+	{
+		// Listen to all events for illustration purposes
+		iCloudManager.keyValueStoreDidChangeEvent += keyValueStoreDidChangeEvent;
+		iCloudManager.ubiquityIdentityDidChangeEvent += ubiquityIdentityDidChangeEvent;
+		iCloudManager.entitlementsMissingEvent += entitlementsMissingEvent;
+		iCloudManager.documentStoreUpdatedEvent += documentStoreUpdatedEvent;
+	}
+
+
+	void OnDisable()
+	{
+		// Remove all event handlers
+		iCloudManager.keyValueStoreDidChangeEvent -= keyValueStoreDidChangeEvent;
+		iCloudManager.ubiquityIdentityDidChangeEvent -= ubiquityIdentityDidChangeEvent;
+		iCloudManager.entitlementsMissingEvent -= entitlementsMissingEvent;
+		iCloudManager.documentStoreUpdatedEvent -= documentStoreUpdatedEvent;
+	}
+
+
+
+	void keyValueStoreDidChangeEvent( List<object> keys )
+	{
+		Debug.Log( "keyValueStoreDidChangeEvent.  changed keys: " );
+		foreach( var key in keys )
+			Debug.Log( key );
+	}
+	
+	
+	void ubiquityIdentityDidChangeEvent()
+	{
+		Debug.Log( "ubiquityIdentityDidChangeEvent" );
+	}
+	
+	
+	void entitlementsMissingEvent()
+	{
+		Debug.Log( "entitlementsMissingEvent" );
+	}
+	
+	
+	void documentStoreUpdatedEvent( List<iCloudManager.iCloudDocument> files )
+	{
+		foreach( var doc in files )
+			Debug.Log( doc );
+	}
+#endif
+}
+
+
